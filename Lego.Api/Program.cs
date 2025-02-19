d02ece73260b2014a3a2c000cae490c34fc0096d
Morten Schou Gross <mgr@dst.dk>
2025-02-19 09:54:03 +0100

    Installed Serilog

--- Lego.Api/Program.cs
+++ Lego.Api/Program.cs
@@ -1,3 +1,11 @@
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/legoApi.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
