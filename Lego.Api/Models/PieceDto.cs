e52f34e3ae07b85f7c41feff8f1bfb31f00178bc
Morten Schou Gross <mgr@dst.dk>
2025-02-27 15:59:28 +0100

    updated comments

--- Lego.Api/Models/PieceDto.cs
+++ Lego.Api/Models/PieceDto.cs
@@ -1,27 +1,27 @@
 ﻿namespace Lego.Api.Models
 {
     /// <summary>
-    /// A Lego part
+    /// A Lego piece
     /// </summary>
     public class PieceDto
     {
         /// <summary>
-        /// The id of the part
+        /// The id of the piece
         /// </summary>
         public int Id { get; set; }
 
         /// <summary>
-        /// The part number of the part
+        /// The part number of the piece
         /// </summary>
         public string PieceNo { get; set; } = string.Empty;
 
         /// <summary>
-        /// The color of the part
+        /// The color of the piece
         /// </summary>
         public string Color { get; set; } = string.Empty;
 
         /// <summary>
-        /// The description of the part
+        /// The description of the piece
         /// </summary>
         public string Description { get; set; } = string.Empty;
     }
