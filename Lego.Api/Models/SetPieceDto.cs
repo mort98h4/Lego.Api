c7ef2c79d38cfdcd125ba3a728ab930bb3e9a045
Morten Schou Gross <mgr@dst.dk>
2025-02-28 12:46:55 +0100

    renamed SetMissingPieceIdDto --> SetPieceDto

--- Lego.Api/Models/SetPieceDto.cs
+++ Lego.Api/Models/SetPieceDto.cs
@@ -1,13 +1,18 @@
 ﻿namespace Lego.Api.Models
 {
     /// <summary>
-    /// A missing piece of a Lego set
+    /// A piece of a Lego set
     /// </summary>
     public class SetMissingPieceIdDto
     {
         /// <summary>
-        /// The id of the missing piece
+        /// The id of the piece
         /// </summary>
         public int PieceId { get; set; }
+
+        /// <summary>
+        /// The quantity of this type of piece
+        /// </summary>
+        public int Quantity { get; set; }
     }
 }
